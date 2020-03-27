798a5864001f9111ac1381a6661246f008e48dec
tabholt <tabholt@gmail.com>
2020-03-27 21:45:20 +0100

    added the generator functions to script

--- data_loader_two_by_two.py
+++ data_loader_two_by_two.py
@@ -77,16 +77,16 @@ def get_data_sets():
         ]),
     ]
 
-    # def training_set():
-    #     while True:
-    #         index = np.random.choice(len(examples))
-    #         yield examples[index]
+    def training_set():
+        while True:
+            index = np.random.choice(len(examples))
+            yield examples[index]
 
-    # def evaluation_set():
-    #     while True:
-    #         index = np.random.choice(len(examples))
-    #         yield examples[index]
+    def evaluation_set():
+        while True:
+            index = np.random.choice(len(examples))
+            yield examples[index]
     
-    # return training_set, evaluation_set
+    return training_set, evaluation_set
 
 
