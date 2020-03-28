12018a57f6afb2376d886d23883280fe3ea17857
tabholt <tabholt@gmail.com>
2020-03-28 11:03:09 +0100

    added iteration loops to framework

--- nn_framework/framework.py
+++ nn_framework/framework.py
@@ -3,9 +3,15 @@ import numpy as np
 class ANN(object):
     def __init__(self, model=None):
         self.layers = model
+        self.n_iter_train = int(1e3)
+        self.n_iter_evaluate = int(1e3)
     
     def train(self, training_set=None):
-        pass
+        for i in range(self.n_iter_train):
+            x = next(training_set()).ravel()
+            print(x)
 
     def evaluate(self, evaluation_set=None):
-        pass
+        for i in range(self.n_iter_evaluate):
+            x = next(evaluation_set()).ravel()
+            print(x)
