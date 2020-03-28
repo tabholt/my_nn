ba84fb746bdc74abccddce275e9372820b4c8b1f
tabholt <tabholt@gmail.com>
2020-03-28 10:50:12 +0100

    finished excercise 4

---
+++ nn_framework/framework.py
@@ -0,0 +1,11 @@
+import numpy as np 
+
+class ANN(object):
+    def __init__(self, model=None):
+        self.layers = model
+    
+    def train(self, training_set=None):
+        pass
+
+    def evaluate(self, evaluation_set=None):
+        pass
--- run_framework.py
+++ run_framework.py
@@ -1,8 +1,13 @@
-import data_loader_two_by_two as dat 
+import data_loader_two_by_two as dat
+import nn_framework.framework as framework
 
 training_set, evaluation_set = dat.get_data_sets()
 
 sample = next(training_set())
 n_pixels = sample.shape[0] * sample.shape[1]
 n_nodes = [n_pixels, n_pixels]
+
+autoencoder = framework.ANN()
+autoencoder.train(training_set)
+autoencoder.evaluate(evaluation_set)
 
