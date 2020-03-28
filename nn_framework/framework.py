8562a0ec1f77aaab2c2c80141a65001608a14bcf
tabholt <tabholt@gmail.com>
2020-03-28 11:57:56 +0100

    added normalization function

--- nn_framework/framework.py
+++ nn_framework/framework.py
@@ -1,17 +1,37 @@
-import numpy as np 
+import numpy as np
+
 
 class ANN(object):
-    def __init__(self, model=None):
+    def __init__(self, model=None, pixel_range=[-.5, .5]):
         self.layers = model
-        self.n_iter_train = int(1e3)
-        self.n_iter_evaluate = int(1e3)
-    
+        self.n_iter_train = int(1e1)
+        self.n_iter_evaluate = int(1e1)
+        self.pixel_range = pixel_range
+
     def train(self, training_set=None):
         for i in range(self.n_iter_train):
             x = next(training_set()).ravel()
+            x = self.normalize(x)
             print(x)
 
     def evaluate(self, evaluation_set=None):
         for i in range(self.n_iter_evaluate):
             x = next(evaluation_set()).ravel()
+            x = self.normalize(x)
             print(x)
+
+    def normalize(self, pic):
+        """
+        Transform the input picture so that all pixel values fall 
+        between the desired pixel_range
+        """
+        high = np.max(pic)
+        low = np.min(pic)
+        dist = high-low
+
+        # return a vector with each value as a percentage between max and min
+        pic_percent = (pic - low) / dist
+        # transform the percentage vector to desired pixel range
+        pic_normalized = pic_percent * \
+            (self.pixel_range[1]-self.pixel_range[0]) + self.pixel_range[0]
+        return pic_normalized
--- run_framework.py
+++ run_framework.py
@@ -7,7 +7,7 @@ sample = next(training_set())
 n_pixels = sample.shape[0] * sample.shape[1]
 n_nodes = [n_pixels, n_pixels]
 
-autoencoder = framework.ANN()
+autoencoder = framework.ANN(model=None, pixel_range=[-.5, .5])
 autoencoder.train(training_set)
 autoencoder.evaluate(evaluation_set)
 
