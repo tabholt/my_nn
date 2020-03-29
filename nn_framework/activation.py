a73fdd2f88a6eb1a55e6b53e758920fba9096909
tabholt <tabholt@gmail.com>
2020-03-29 19:22:08 +0200

    added logistic and relu activation functions

--- nn_framework/activation.py
+++ nn_framework/activation.py
@@ -3,5 +3,32 @@ import numpy as np
 
 class tanh(object):
     @staticmethod
-    def calc(alpha):
-        return np.tanh(alpha)
+    def calc(v):
+        return np.tanh(v)
+
+    @staticmethod
+    def calc_d(v):
+        return 1 - np.tanh(v)**2
+
+
+class logistic(object):
+    @staticmethod
+    def calc(v):
+        return 1/(1 + np.exp(-v))
+
+    @staticmethod
+    def calc_d(v):
+        fv = 1/(1 + np.exp(-v))
+        return fv * (1-fv)
+
+
+class relu(object):
+    @staticmethod
+    def calc(v):
+        return np.maximum(0, v)
+
+    @staticmethod
+    def calc_d(v):
+        d = np.zeros(v.shape)
+        d[np.where(v > 0)] = 1
+        return d
