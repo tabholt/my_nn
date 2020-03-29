3a05eb0b713090ea5edcbd9f8522a3a682c633d9
tabholt <tabholt@gmail.com>
2020-03-29 13:57:38 +0200

    added reporting functionality

--- nn_framework/framework.py
+++ nn_framework/framework.py
@@ -1,27 +1,50 @@
+import os
 import numpy as np
+import matplotlib.pyplot as plt
+plt.switch_backend("agg")
 
 
 class ANN(object):
     def __init__(self, model, normalized_pixel_range=[-.5, .5], input_pixel_range=[np.nan, np.nan]):
         self.layers = model
-        self.n_iter_train = int(1e1)
-        self.n_iter_evaluate = int(1e1)
+        self.error_history = []
+        self.n_iter_train = int(1e6)
+        self.n_iter_evaluate = int(1e6)
+        self.viz_interval = int(1e5)
+        self.reporting_bin_size = int(1e3)
+        self.report_min = -3
+        self.report_max = 0
+
         self.normalized_pixel_range = normalized_pixel_range
         self.input_pixel_range = input_pixel_range
+
+        self.reports_path = 'reports'
+        self.report_name = 'performance_history.png'
+
+        try:
+            os.mkdir(self.reports_path)
+        except:
+            pass
 
     def train(self, training_set):
         for i in range(self.n_iter_train):
             x = next(training_set()).ravel()
             x = self.normalize(x)
             y = self.forward_prop(x)
-            print(y)
+            self.error_history.append(1)
+
+            if (i+1) % self.viz_interval == 0:
+                self.report()
 
     def evaluate(self, evaluation_set):
         for i in range(self.n_iter_evaluate):
             x = next(evaluation_set()).ravel()
             x = self.normalize(x)
             y = self.forward_prop(x)
-            print(y)
+            self.error_history.append(1)
+
+            if (i+1) % self.viz_interval == 0:
+                self.report()
 
     def normalize(self, pic):
         """
@@ -57,3 +80,25 @@ class ANN(object):
         for layer in self.layers:
             y = layer.forward_prop(y)
         return y.ravel()
+
+    def report(self):
+        n_bins = int(len(self.error_history)) // self.reporting_bin_size
+        smoothed_history = []
+        for i_bin in range(n_bins):
+            smoothed_history.append(np.mean(self.error_history[
+                i_bin * self.reporting_bin_size:
+                (i_bin+1) * self.reporting_bin_size
+            ]))
+        error_history = np.log10(np.array(smoothed_history)+1e-10)
+        ymin = np.minimum(self.report_min, np.min(error_history))
+        ymax = np.maximum(self.report_max, np.max(error_history))
+
+        fig = plt.figure()
+        ax = plt.gca()
+        ax.plot(error_history)
+        ax.set_xlabel('x%d iterations' % (self.reporting_bin_size))
+        ax.set_ylabel('log error')
+        ax.set_ylim(ymin, ymax)
+        ax.grid()
+        fig.savefig(os.path.join(self.reports_path, self.report_name))
+        plt.close()
